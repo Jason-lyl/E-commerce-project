5680ac3b8ae2c1293b19103975db6eccb1e84eb3
Jason012 <longge_li0123@163.com>
2019-09-17 13:57:16 +0800

    spring 、springmvc configuration

---
+++ src/main/java/com/mmall/dao/testIdMapper.java
@@ -0,0 +1,9 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.testId;
+
+public interface testIdMapper {
+    int insert(testId record);
+
+    int insertSelective(testId record);
+}
---
+++ src/main/java/com/mmall/pojo/testId.java
@@ -0,0 +1,21 @@
+package com.mmall.pojo;
+
+public class testId {
+    private Integer id;
+
+    public testId(Integer id) {
+        this.id = id;
+    }
+
+    public testId() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
