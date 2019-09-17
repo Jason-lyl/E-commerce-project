4e021a3c3c858c13f3950cde99b8f4435638f68f
Jason012 <longge_li0123@163.com>
2019-09-17 18:46:14 +0800

    user module

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,16 @@
+package com.mmall.common;
+
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMALI = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role{
+
+        int ROLE_CUSTOMER = 0;//普通用户
+        int Role_ADMIN = 1; //管理员
+    }
+
+}
